0208f9fc1ab1d914a89529c21159d93a00ff53c2
anbecodes <anbecodes@gmail.com>
2021-03-11 22:24:49 +0100

    bug fixed

--- app.py
+++ app.py
@@ -7,15 +7,23 @@ display = Entry(graphic)
 display.grid(row=1, columnspan=6, sticky=W+E)
 
 # number buttons
-Button(graphic, text=1,).grid(row=2, column=0)
-Button(graphic, text=2,).grid(row=2, column=1)
-Button(graphic, text=3,).grid(row=2, column=2)
-Button(graphic, text=4,).grid(row=3, column=0)
-Button(graphic, text=5,).grid(row=3, column=1)
-Button(graphic, text=6,).grid(row=3, column=2)
-Button(graphic, text=7,).grid(row=4, column=0)
-Button(graphic, text=8,).grid(row=4, column=1)
-Button(graphic, text=9,).grid(row=4, column=2)
-Button(graphic, text=0,).grid(row=5, column=0)
+
+# row one 
+Button(graphic, text="1").grid(row=2, column=0)
+Button(graphic, text="2").grid(row=2, column=1)
+Button(graphic, text="3").grid(row=2, column=2)
+
+# row two
+Button(graphic, text="4").grid(row=3, column=0)
+Button(graphic, text="5").grid(row=3, column=1)
+Button(graphic, text="6").grid(row=3, column=2)
+
+# row three
+Button(graphic, text="7").grid(row=4, column=0)
+Button(graphic, text="8").grid(row=4, column=1)
+Button(graphic, text="9").grid(row=4, column=2)
+
+# row five
+Button(graphic, text="0").grid(row=5, column=0)
 
 graphic.mainloop()
