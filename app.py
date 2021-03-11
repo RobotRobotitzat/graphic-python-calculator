6eed182e2a57279c7ce0af26ff7692fa220b1f24
anbecodes <anbecodes@gmail.com>
2021-03-11 22:05:00 +0100

    first button

--- app.py
+++ app.py
@@ -1,4 +1,12 @@
 from tkinter import *
 
-root = Tk()
-root.mainloop()
+graphic = Tk()
+graphic.title("Calculator")
+
+display = Entry(graphic)
+display.grid(row=1, columnspan=6, sticky=W+E)
+
+# number buttons
+Button(graphic, text=1,).grid(row=2)
+
+graphic.mainloop()
