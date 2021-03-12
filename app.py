123d43ec5b0191f67cf65ddc50b6192fa3f34b3b
Robot <tonteriesvaries1@gmail.com>
2021-03-12 22:56:45 +0100

    add other buttons for operations

--- app.py
+++ app.py
@@ -32,6 +32,13 @@ Button(graphic, text="0").grid(row=5, column=1, sticky=W+E)
 Button(graphic, text="AC").grid(row=5, column=2, sticky=W+E)
 Button(graphic, text="%").grid(row=5, column=0, sticky=W+E)
 
+Button(graphic, text="←").grid(row=2, column=4, sticky=W+E, columnspan=2)
+Button(graphic, text="EXP").grid(row=3, column=4, sticky=W+E)
+Button(graphic, text="^2").grid(row=4, column=4, sticky=W+E)
+Button(graphic, text="(").grid(row=5, column=3, sticky=W+E)
+Button(graphic, text=")").grid(row=5, column=3, sticky=W+E)
+Button(graphic, text="=").grid(row=5, column=3, sticky=W+E)
+
 # operation buttons
 Button(graphic, text="+").grid(row=2, column=3, sticky=W+E)
 Button(graphic, text="-").grid(row=3, column=3, sticky=W+E)
