e85963a7884dc5cc498a60bbd182f9cbe5cfbfe5
Robot <tonteriesvaries1@gmail.com>
2021-03-12 22:30:57 +0100

    modify the coments

--- app.py
+++ app.py
@@ -8,22 +8,22 @@ display.grid(row=1, columnspan=6, sticky=W+E)
 
 # number buttons
 
-# row one 
+# number buttons row one 
 Button(graphic, text="1").grid(row=2, column=0)
 Button(graphic, text="2").grid(row=2, column=1)
 Button(graphic, text="3").grid(row=2, column=2)
 
-# row two
+# number buttons row two
 Button(graphic, text="4").grid(row=3, column=0)
 Button(graphic, text="5").grid(row=3, column=1)
 Button(graphic, text="6").grid(row=3, column=2)
 
-# row three
+# number buttons row three
 Button(graphic, text="7").grid(row=4, column=0)
 Button(graphic, text="8").grid(row=4, column=1)
 Button(graphic, text="9").grid(row=4, column=2)
 
-# row five
+# number buttons row five
 Button(graphic, text="0").grid(row=5, column=1)
 
 
