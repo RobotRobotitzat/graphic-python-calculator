f3267198fbf5687de6a56fa3454c6e2991c67f92
Robot <tonteriesvaries1@gmail.com>
2021-03-12 22:27:01 +0100

    operation buttons created the first

--- app.py
+++ app.py
@@ -32,4 +32,7 @@ Button(graphic, text="0").grid(row=5, column=1)
 Button(graphic, text="AC").grid(row=5, column=2)
 Button(graphic, text="%").grid(row=5, column=0)
 
+# operation buttons
+Button(graphic, text="").grid(row=, column=)
+
 graphic.mainloop()
